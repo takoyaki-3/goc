5130c6c38aa012465d2c008c39d7d215f5b02394
takoyaki-3 <mail@takoyaki-3.com>
2020-12-29 17:12:14 +0900

    CSV分割関数を追加

--- goc.go
+++ goc.go
@@ -3,6 +3,7 @@ package goc
 import (
 	"os"
 	"io"
+	"log"
 	"fmt"
 	"bufio"
 	"io/ioutil"
@@ -127,4 +128,62 @@ func BOMCsvReader(r io.Reader) *csv.Reader {
 		br.Discard(3)
 	}
 	return csv.NewReader(br)
+}
+
+func SpiritCSV(filename string,outputpath string,column string){
+
+	buf := map[string][][]string{}
+
+	// Make directory
+	os.Mkdir(outputpath, 0777)
+
+	// Open read table file
+	rf, rerr := os.Open(filename)
+	if rerr != nil {
+		fmt.Println("[" + filename + "] is not found")
+	}
+	defer rf.Close()
+	reader := BOMCsvReader(rf)
+	reader.FieldsPerRecord = -1
+
+	counter := -1
+	titles := map[string]int{}
+	first_line := []string{}
+
+	for {
+		counter++
+		line, er := reader.Read()
+		if er != nil {
+			break
+		}
+		if counter==0{
+			first_line = line
+			for k,v := range line{
+				titles[v]=k
+			}
+			continue
+		}
+		id := line[titles[column]]
+		if _,ok := buf[id]; !ok{
+			buf[id] = [][]string{first_line}
+		}
+		if len(first_line) == len(line){
+			buf[id] = append(buf[id],line)
+		}
+	}
+
+	// Output files
+	for k,v := range buf{
+		// Open write table file
+		wf, werr := os.Create("./splited/" + outputpath + "/" + k + ".csv")
+		if werr != nil {
+			log.Fatal(werr)
+		}
+		writer := csv.NewWriter(wf)
+		for _,line := range v{
+			writer.Write(line)
+		}
+		writer.Flush()
+		wf.Close()
+	}
 }
